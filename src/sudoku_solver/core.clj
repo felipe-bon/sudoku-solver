d9c4e526f96946cbf8e0e4b81ddcc5a2420b0878
Davi Vilarinho <davi.r.vilarinho@gmail.com>
2022-11-26 10:32:39 -0300

    tests

--- src/sudoku_solver/core.clj
+++ src/sudoku_solver/core.clj
@@ -1,61 +1,31 @@
 (ns sudoku-solver.core
-  (:gen-class)
-  (:require [sudoku-io]))
+  (:gen-class))
 
-(def SUDOKU-SIZE 9)
+(def SUDOKU-LINE-SIZE 9)
 (def SUDOKU-GRID 3)
+(def SUDOKU-SIZE (* SUDOKU-LINE-SIZE SUDOKU-LINE-SIZE))
 
-(defn line [v]
-  (let [first-pos-line (* SUDOKU-SIZE
-                          (quot v SUDOKU-SIZE))]
-    (remove #(= % v)
-            (range first-pos-line
-                   (+ SUDOKU-SIZE
-                      first-pos-line)))))
+(defn same-line [v w] (= (quot v SUDOKU-LINE-SIZE)
+                         (quot w SUDOKU-LINE-SIZE)))
 
-(defn column [v]
-  (let [first-column (quot v SUDOKU-SIZE)]
-    (remove #(= % v)
-            (map #(+ % (mod v SUDOKU-SIZE))
-                 (range 0
-                        (* SUDOKU-SIZE SUDOKU-SIZE)
-                        SUDOKU-SIZE)))))
-; 12 13 14
-; 21 22 23
-; 30 31 32
-(defn subgrid [v]
-  (let [first-element-of-grid-line
-        (* (quot v (* SUDOKU-GRID
-                      SUDOKU-SIZE))
-           (* SUDOKU-SIZE SUDOKU-GRID))
-        mod-size-min-threshold (* (quot (mod v SUDOKU-SIZE)
-                                        SUDOKU-GRID)
-                                  SUDOKU-GRID)]
-    (filter #(and (not= % v)
-                  (<= mod-size-min-threshold
-                      (mod % SUDOKU-SIZE)
-                      (dec (+ mod-size-min-threshold SUDOKU-GRID))))
-            (range first-element-of-grid-line
-                   (+ first-element-of-grid-line
-                      (* SUDOKU-GRID
-                         SUDOKU-SIZE))))))
+(defn same-coll [v w] (= (mod v SUDOKU-LINE-SIZE)
+                         (mod w SUDOKU-LINE-SIZE)))
 
-(defn line [v]
-  (let [first-pos-line (- v
-                          (mod v SUDOKU-SIZE))]
-    (range first-pos-line
-           (+ SUDOKU-SIZE
-              first-pos-line))))
+(defn which-subgrid [v] {:x (quot (mod v SUDOKU-LINE-SIZE)
+                                   SUDOKU-GRID)
+                         :y (quot v (* SUDOKU-GRID SUDOKU-LINE-SIZE))})
 
-(defn sudoku-chuveirinho []
-  (map #(into (sorted-set-by <) (conj (concat (subgrid %)
-                                             (column %)
-                                             (line %))
-                                     %))
-       (range 0
-              (* SUDOKU-SIZE SUDOKU-SIZE))))
+(defn same-subgrid [v w] (= (which-subgrid v)
+                            (which-subgrid w)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn is-adj
+  [v w] (or (same-line v w)
+            (same-coll v w)
+            (same-subgrid v w)))
+
+(defn sudoku-chuveirinho
+  ([] (sudoku-chuveirinho 0))
+  ([v] (if (< v SUDOKU-SIZE) (concat (list (remove #(not (is-adj v %))
+                                                   (range 0 SUDOKU-SIZE)))
+                                     (sudoku-chuveirinho (inc v))))))
+
