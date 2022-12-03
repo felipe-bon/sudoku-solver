a9d5449d027ff00f3838d02bca39654461b9bdd1
Davi Vilarinho <davi.r.vilarinho@gmail.com>
2022-12-03 16:50:36 -0300

    default sudoku

--- src/main/java/ufuvbr/sudokusolver/SudokuGameSimpleFactory.java
+++ src/main/java/ufuvbr/sudokusolver/SudokuGameSimpleFactory.java
@@ -11,6 +11,36 @@ public class SudokuGameSimpleFactory {
         for (int i = 0; i < 81; i++) {
             coloring.add(null);
         }
+        coloring.set(0, 5);
+        coloring.set(1, 5);
+        coloring.set(4, 7);
+        coloring.set(9, 6);
+        coloring.set(12, 1);
+        coloring.set(13, 9);
+        coloring.set(14, 5);
+        coloring.set(19, 9);
+        coloring.set(20, 8);
+        coloring.set(25, 6);
+        coloring.set(27, 8);
+        coloring.set(31, 6);
+        coloring.set(35, 3);
+        coloring.set(36, 4);
+        coloring.set(39, 8);
+        coloring.set(41, 3);
+        coloring.set(44, 1);
+        coloring.set(45, 7);
+        coloring.set(49, 2);
+        coloring.set(53, 6);
+        coloring.set(55, 6);
+        coloring.set(60, 2);
+        coloring.set(61, 8);
+        coloring.set(66, 4);
+        coloring.set(67, 1);
+        coloring.set(68, 9);
+        coloring.set(71, 5);
+        coloring.set(76, 8);
+        coloring.set(79, 7);
+        coloring.set(80, 9);
         return coloring;
     }
     public static SudokuGame createSudoku(Class<? extends SolverStrategy> solverStrategyClass) {
